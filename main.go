f4def7cf9fd7968cfda8fc1d2a71fe694c7130e0
kctjohnson <killeanjohnson06@gmail.com>
2022-09-01 13:11:40 +0000

    Restore main file

--- main.go
+++ main.go
@@ -1,18 +1,16 @@
 package main
 
 import (
-	// "github.com/kctjohnson/bubble-boids/cmd/cli"
-	"github.com/kctjohnson/bubble-boids/cmd/pixel"
-	// "github.com/kctjohnson/bubble-boids/cmd/server"
+	"github.com/kctjohnson/bubble-boids/cmd/cli"
+	"github.com/kctjohnson/bubble-boids/cmd/server"
 )
 
 const servermode = false
 
 func main() {
-	pixel.Execute()
-	// if servermode {
-	// 	server.Execute()
-	// } else {
-	// 	cli.Execute()
-	// }
+	if servermode {
+		server.Execute()
+	} else {
+		cli.Execute()
+	}
 }
