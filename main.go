b4fec7b5c1118135d9ccc4ae67b82f9b02ae901d
kctjohnson <killeanjohnson06@gmail.com>
2022-09-01 18:12:28 +0000

    Restored main file

--- main.go
+++ main.go
@@ -1,9 +1,6 @@
 package main
 
 import (
-	"net/http"
-	_ "net/http/pprof"
-
 	"github.com/kctjohnson/bubble-boids/cmd/cli"
 	"github.com/kctjohnson/bubble-boids/cmd/server"
 )
@@ -11,9 +8,6 @@ import (
 const servermode = false
 
 func main() {
-	go func() {
-		http.ListenAndServe(":8080", nil)
-	}()
 	if servermode {
 		server.Execute()
 	} else {
